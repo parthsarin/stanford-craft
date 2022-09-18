c26806cb7dfb662280ca9eac296aede05a735176
Parth Sarin <psarin@stanford.edu>
2022-09-18 10:14:16 +0200

    initialize, with firebase

---
+++ src/App/App.tsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="app">
+      <h1>AI Lit</h1>
+    </div>
+  );
+}
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCDCTEbILAwX5OYe7xWEfKT87EEoK6cDjc",
+  authDomain: "ai-lit.firebaseapp.com",
+  projectId: "ai-lit",
+  storageBucket: "ai-lit.appspot.com",
+  messagingSenderId: "1062536378260",
+  appId: "1:1062536378260:web:4bdbad82dc90f6f39f8181",
+  measurementId: "G-NCC0620GMK"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+getAnalytics(app);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
