1d08bea7b8f9618ddd49d6f6df546b8a5b302fb9
Parth Sarin <psarin@stanford.edu>
2023-02-14 11:11:44 -0800

    add matchResource search function

--- src/Dashboard/Resources/Resource.ts
+++ src/Dashboard/Resources/Resource.ts
@@ -12,4 +12,24 @@ interface ResourceSubLink {
   url: string;
 }
 
+const matchResource = (resource: Resource, query: string): boolean => {
+  const tokens = query
+                  .toLowerCase()
+                  .split(' ')
+                  .filter((t) => t.trim() !== '');
+  
+  // we can return to this (below) or some semantic option later
+  // query = query.toLowerCase();
+  // query = '.*' + query.split('').join('.*') + '.*';
+  
+  const regex = new RegExp(tokens.join('|'), 'gim');
+
+  const { title, description, subLinks, tags } = resource;
+  const names = subLinks.map((subLink) => subLink.name);
+  const all = [title, description, ...names, ...tags].join(' ').toLowerCase();
+
+  return regex.test(all);
+}
+
 export type { Resource, ResourceSubLink };
+export { matchResource };
